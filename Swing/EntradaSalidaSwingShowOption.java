7b32e7a8917cc421a2b36831ee02dd20a5f91226
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-11-02 13:03:48 +0100

    Create EntradaSalidaSwingShowOption.java

---
+++ Swing/EntradaSalidaSwingShowOption.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class EntradaSalidaSwingShowOption {
+
+	public static void main(String[] args) {
+
+		Object[] opciones = {"Opción A", "Opción B", "Opción C"};
+
+		int opcion = JOptionPane.showOptionDialog(null, "Selecciona una de las siguientes opciones:",
+				"Swing", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+		JOptionPane.showMessageDialog(null, "Has elegido " + opciones[opcion]);
+	}
+}
