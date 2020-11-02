4d6c1e98416f75942a8b065271829ef409bbdaa8
María González Herrero <43043718+mgh99@users.noreply.github.com>
2020-11-02 13:03:18 +0100

    Create EntradaSalidaSwingShowMessage.java

---
+++ Swing/EntradaSalidaSwingShowMessage.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class EntradaSalidaSwingShowMessage {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null,
+				"Hola, este es mi primer programa Java con Swing");
+
+		JOptionPane.showMessageDialog(null,
+				"Este es un mensaje de error crítico", "Swing",
+				JOptionPane.ERROR_MESSAGE, null);
+
+		JOptionPane.showMessageDialog(null,
+				"Este es un mensaje informativo", "Swing",
+				JOptionPane.INFORMATION_MESSAGE, null);
+
+		JOptionPane.showMessageDialog(null,
+				"Este es un mensaje de advertencia", "Swing",
+				JOptionPane.WARNING_MESSAGE, null);
+
+		JOptionPane.showMessageDialog(null,
+				"Este es un mensaje de interrogación", "Swing",
+				JOptionPane.QUESTION_MESSAGE, null);
+
+	}
+}
