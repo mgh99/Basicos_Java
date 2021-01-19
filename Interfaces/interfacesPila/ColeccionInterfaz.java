97e988308c0317e0b4b5363e973eaf9af1e69609
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-19 21:41:33 +0100

    Create ColeccionInterfaz.java

---
+++ Interfaces/interfacesPila/ColeccionInterfaz.java
@@ -0,0 +1,11 @@
+package interfaces.pila;
+
+public interface ColeccionInterfaz {
+	
+	public boolean estaVacia();
+	public Object extraer();
+	public Object primero();
+	public boolean añadir(Object objeto);
+	
+
+}
