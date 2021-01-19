4f92dfbb1f177b445111448da2db4a04be6185d6
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-19 19:52:52 +0100

    Create IConjunto.java

---
+++ Interfaces/Interfaces2/IConjunto.java
@@ -0,0 +1,10 @@
+package Interfaces2;
+
+public interface IConjunto {
+	
+	public boolean vacio();
+	public void inserta (String clave);
+	public boolean contiene(String clave);
+	public void inicializa();
+
+}
