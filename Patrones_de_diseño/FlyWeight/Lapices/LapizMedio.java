65e308dcc0219bb6fed9b7affd632e2fac38c3c5
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 20:40:43 +0100

    Create LapizMedio.java

---
+++ Patrones_de_diseño/FlyWeight/Lapices/LapizMedio.java
@@ -0,0 +1,16 @@
+package patrones.FlyWeight_lapices;
+
+public class LapizMedio implements Lapiz {
+	
+	final GrosorLapiz grosor = GrosorLapiz.MEDIO;
+	private ColorLapiz color = null;
+	
+	public void setColor(ColorLapiz color) {
+		this.color = color;
+	}
+	
+	@Override
+	public void escribe(String texto) {
+		System.out.println("Escribiendo con el lapiz medio " + color + ": " + texto);
+	}
+}
