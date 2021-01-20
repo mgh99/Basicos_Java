6e22c1bca8ec89beaa3f3cd649c3ad4168ad5ed3
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 16:19:51 +0100

    Create Bateria.java

---
+++ Patrones_de_diseño/Facade/Movil/Bateria.java
@@ -0,0 +1,13 @@
+package patrones.Facade_Movil;
+
+public class Bateria {
+	
+	public void on() {
+		System.out.println("Batería on");
+	}
+	
+	public void off() {
+		System.out.println("Batería off");
+	}
+
+}
