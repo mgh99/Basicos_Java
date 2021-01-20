3157aab13526305e94cf9e35b9ab48712ec1e383
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 11:29:36 +0100

    Create Cuadrado.java

---
+++ Patrones_de_diseño/Factory/Formas_Geometricas/Cuadrado.java
@@ -0,0 +1,10 @@
+package patrones.Factory_FigurasGeometricas;
+
+public class Cuadrado implements Forma{ 
+	
+	@Override 
+	public void dibujar() {
+		System.out.println("Soy un cuadrado");
+	}
+
+}
