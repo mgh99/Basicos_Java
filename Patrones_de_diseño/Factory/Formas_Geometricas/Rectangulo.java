cefc74153626c457fd8bf4ce23d5a47b52e702a2
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 11:30:42 +0100

    Create Rectangulo.java

---
+++ Patrones_de_diseño/Factory/Formas_Geometricas/Rectangulo.java
@@ -0,0 +1,10 @@
+package patrones.Factory_FigurasGeometricas;
+
+public class Rectangulo implements Forma{
+	
+	@Override
+	public void dibujar() {
+		System.out.println("Soy un rectángulo");
+	}
+
+}
