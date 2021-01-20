11d9b1d05ab4a8ca2279a6d029f7ed55b5829da8
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 20:17:58 +0100

    Create Calefaccion.java

---
+++ Patrones_de_diseño/Command/Calefaccion/Calefaccion.java
@@ -0,0 +1,20 @@
+package patrones.Command_calefaccion;
+
+public class Calefaccion {
+	
+	private String tipo;
+	
+	public Calefaccion(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public void encender() {
+		System.out.println("Encendiendo la calefacción " +
+				this.tipo);
+	}
+	
+	public void apagar() {
+		System.out.println("Apagando la calefacción " +
+				this.tipo);
+	}
+}
