b8600932d094668823e67d89329c532b6b23cc67
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 18:53:42 +0100

    Create InvestigadorTitular.java

---
+++ Patrones_de_diseño/MVC/Investigadores/InvestigadorTitular.java
@@ -0,0 +1,28 @@
+package patrones.MVC_Investigacion;
+
+import java.time.LocalDate;
+
+public class InvestigadorTitular extends Investigador {
+	
+	private int sexenios;
+	
+	public InvestigadorTitular(String nif, String nombre, String apellidos, LocalDate fechaContratacion,
+			double retribucion, int sexenios) {
+		
+		super(nif, nombre, apellidos, fechaContratacion, retribucion);
+		this.sexenios = sexenios;
+	}
+	
+	public int getSexenios() {
+		return this.sexenios;
+	}
+	
+	public double getSubidaSalarial() {
+		double factor = 0.035 + this.sexenios/100.0 + ((super.getAntigüedad() >= 5) ? 0.01 : 0.0);
+		return (super.getRetribucion() * factor);
+	}
+	
+	public String getDatos() {
+		return super.getDatos() + " con " + this.sexenios + " sexenios";
+	}
+}
