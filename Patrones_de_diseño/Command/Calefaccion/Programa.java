8ab10117d853a84d3aec15070526b6f697f072cd
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 20:20:57 +0100

    Create Programa.java

---
+++ Patrones_de_diseño/Command/Calefaccion/Programa.java
@@ -0,0 +1,20 @@
+package patrones.Command_calefaccion;
+
+public class Programa {
+	
+	public static void main(String args[]) {
+		
+		Calefaccion electrica = new Calefaccion("Electrica");
+		Calefaccion gas = new Calefaccion("Gas");
+		MandoDistancia mando = new MandoDistancia();
+		
+		mando.comando(new CalefaccionEncender(electrica));
+		mando.ejecuta();
+		mando.comando(new CalefaccionEncender(gas));
+		mando.ejecuta();
+		mando.comando(new CalefaccionApagar(electrica));
+		mando.ejecuta();
+		mando.comando(new CalefaccionApagar(gas));
+		mando.ejecuta();
+	}
+}
