4349236f67121bcce77b2d9260db35862a387b1a
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 17:23:40 +0100

    Create PedidoEntregado.java

---
+++ Patrones_de_diseño/State/Pedidos/PedidoEntregado.java
@@ -0,0 +1,17 @@
+package patrones.State_pedidos;
+
+public class PedidoEntregado implements EstadoPedido {
+	
+	@Override
+	public void siguiente(Pedido p) { }
+	
+	@Override
+	public void anterior(Pedido p) {
+		p.setEstado( new PedidoEnviado() );
+	}
+	
+	@Override
+	public String descripcion() {
+		return "Pedido entregado al cliente";
+	}
+}
