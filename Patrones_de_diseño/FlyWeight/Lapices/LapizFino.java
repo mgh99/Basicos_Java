5170fa13ba95e2f5128a4a31f87af40bdeca165e
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 20:39:35 +0100

    Create LapizFino.java

---
+++ Patrones_de_diseño/FlyWeight/Lapices/LapizFino.java
@@ -0,0 +1,17 @@
+package patrones.FlyWeight_lapices;
+
+public class LapizFino implements Lapiz {
+	
+	final GrosorLapiz grosor = GrosorLapiz.FINO;
+	private ColorLapiz color = null;
+	
+	public void setColor(ColorLapiz color) {
+		this.color = color;
+	}
+	
+	@Override
+	public void escribe(String texto) {
+		System.out.println("Escribiendo con el lapiz fino " +
+				color + ": " + texto);
+	}
+}
