ed9e774f3e8af5af5ce3605102144bfb9161990d
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 17:24:38 +0100

    Create PedidoPreparado.java

---
+++ Patrones_de_diseño/State/Pedidos/PedidoPreparado.java
@@ -0,0 +1,19 @@
+package patrones.State_pedidos;
+
+public class PedidoPreparado implements EstadoPedido {
+	
+	@Override
+	public void siguiente(Pedido p) {
+		p.setEstado( new PedidoEnviado() );
+	}
+	
+	@Override
+	public void anterior(Pedido p) {
+		p.setEstado( new PedidoRecibido() );
+	}
+	
+	@Override
+	public String descripcion() {
+		return " Pedido preparado, listo para enviar";
+	}
+}
