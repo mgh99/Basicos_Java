630d095c885aa1a9dc321cf220a249a1b9dc607e
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-01-20 16:20:14 +0100

    Create CPU.java

---
+++ Patrones_de_diseño/Facade/Movil/CPU.java
@@ -0,0 +1,12 @@
+package patrones.Facade_Movil;
+
+public class CPU {
+	
+	public void puntoArranque() {
+		System.out.println("Punto de arranque del CPU");
+	}
+	
+	public void apagar() {
+		System.out.println("Apagar la CPU");
+	}
+}
